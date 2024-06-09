2e3b5c9bae6da97ae9f01eb2e3a715d9a4fc10cc
Siddhi yadav <113456549+Siddhi1kcr@users.noreply.github.com>
2024-06-09 16:40:55 +0530

    Basic Web setup and creating Home page

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+import {assets} from '../../assets/assets'
+
+const Navbar = () => {
+  
+  const [menu,setMenu] = useState("Home");
+
+  return (
+    <div className='navbar'>
+      <img src={assets.logo} alt='' className='logo'/>
+      <ul className='navbar-menu'>
+        <li onClick={()=>setMenu("Home")} className={menu==="Home"?"active":""}>Home</li>
+        <li onClick={()=>setMenu("Menu")} className={menu==="Menu"?"active":""}>Menu</li>
+        <li onClick={()=>setMenu("About us")} className={menu==="About us"?"active":""}>About us</li>
+        <li onClick={()=>setMenu("Contact us")} className={menu==="Contact us"?"active":""}>Contact us</li>
+      </ul>
+      <div className='navbar-right'>
+        <img src={assets.search_icon}/>
+        <div className='navbar-search-icon'>
+          <img src={assets.basket_icon} alt=''/>
+          <div className='dot'></div>
+        </div>
+        <button className=''>Login</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
