02796c4bf536b880f640b504779b32c8bccfe775
Siddhi yadav <113456549+Siddhi1kcr@users.noreply.github.com>
2024-06-15 18:03:22 +0530

    Integrated backend to frontend for login

--- frontend/src/components/LoginPopUp/LoginPopUp.jsx
+++ frontend/src/components/LoginPopUp/LoginPopUp.jsx
@@ -1,24 +1,61 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import './LoginPopUp.css'
-import { useState } from 'react';
 import { assets } from '../../assets/assets'
+import { StoreContext } from '../../context/StoreContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
-const LoginPopUp = ({setShowLogin}) => {
+const LoginPopup = ({ setShowLogin }) => {
 
-  const [currState, setCurrState] = useState("Sign Up");
+    const { setToken, url,loadCartData } = useContext(StoreContext)
+    const [currState, setCurrState] = useState("Sign Up");
 
-  return (
-    <div className='login-popup'>
-            <form className="login-popup-container">
+    const [data, setData] = useState({
+        name: "",
+        email: "",
+        password: ""
+    })
+
+    const onChangeHandler = (event) => {
+        const name = event.target.name
+        const value = event.target.value
+        setData(data => ({ ...data, [name]: value }))
+    }
+
+    const onLogin = async (e) => {
+        e.preventDefault()
+
+        let new_url = url;
+        if (currState === "Login") {
+            new_url += "/api/user/login";
+        }
+        else {
+            new_url += "/api/user/register"
+        }
+        const response = await axios.post(new_url, data);
+        if (response.data.success) {
+            setToken(response.data.token)
+            localStorage.setItem("token", response.data.token)
+            loadCartData({token:response.data.token})
+            setShowLogin(false)
+        }
+        else {
+            toast.error(response.data.message)
+        }
+    }
+
+    return (
+        <div className='login-popup'>
+            <form onSubmit={onLogin} className="login-popup-container">
                 <div className="login-popup-title">
                     <h2>{currState}</h2> <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt="" />
                 </div>
                 <div className="login-popup-inputs">
-                    {currState === "Sign Up" ? <input type="text" placeholder='Your name' required /> : <></>}
-                    <input type="email" placeholder='Your email' required />
-                    <input type="password" placeholder='Password' required />
+                    {currState === "Sign Up" ? <input name='name' onChange={onChangeHandler} value={data.name} type="text" placeholder='Your name' required /> : <></>}
+                    <input name='email' onChange={onChangeHandler} value={data.email} type="email" placeholder='Your email' />
+                    <input name='password' onChange={onChangeHandler} value={data.password} type="password" placeholder='Password' required />
                 </div>
-                <button>{currState === "Login" ? "Login" : "Create account"}</button>
+                <button> {currState === "Login" ? "Login" : "Create account"}</button>
                 <div className="login-popup-condition">
                     <input type="checkbox" name="" id="" required/>
                     <p>By continuing, i agree to the terms of use & privacy policy.</p>
@@ -29,7 +66,7 @@ const LoginPopUp = ({setShowLogin}) => {
                 }
             </form>
         </div>
-  )
+    )
 }
 
-export default LoginPopUp
+export default LoginPopup
