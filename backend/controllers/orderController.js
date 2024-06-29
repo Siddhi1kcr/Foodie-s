93e7f02d273dc6e9dc507351afb4ad3e8dbeb583
Siddhi yadav <113456549+Siddhi1kcr@users.noreply.github.com>
2024-06-29 17:41:12 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -39,8 +39,8 @@ const placeOrder = async (req, res) => {
         })
         
           const session = await stripe.checkout.sessions.create({
-            success_url: `http://localhost:5173/verify?success=true&orderId=${newOrder._id}`,
-            cancel_url: `http://localhost:5173/verify?success=false&orderId=${newOrder._id}`,
+            success_url: `https://tomato-app-frontend-hm38.onrender.com/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `https://tomato-app-frontend-hm38.onrender.com/verify?success=false&orderId=${newOrder._id}`,
             line_items: line_items,
             mode: 'payment',
           });
